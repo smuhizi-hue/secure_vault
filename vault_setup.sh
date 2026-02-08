a8fce9edc1fc8b9562479f471749daa43f4d24e9
shaun <s.muhizi@alustudent.com>
2026-02-08 17:34:19 +0200

    Fix shebang in vault_setup.sh and add file creation commands

--- vault_setup.sh
+++ vault_setup.sh
@@ -1,1 +1,7 @@
-#!bin/bash
+#!/bin/bash
+mkdir secure_vault
+echo "Welcome to the keys vault." > secure_vault/keys.txt
+echo "Welcome to the secrets vault." > secure_vault/secrets.txt
+echo  "Welcome to the logs vault." > secure_vault/logs.txt
+echo "Success! Files created and ready."
+ls -l secure_vault
